a3a97405ad4ca805cd84d5673d157cb972908c96
Galyaminsky_Linux <jobforandroid@proton.me>
2024-09-16 00:17:28 +0500

    #10 All Repository Completed

---
+++ app/src/main/java/galyaminsky/quotipad/data/AddItemRepoImpl.kt
@@ -0,0 +1,25 @@
+package galyaminsky.quotipad.data
+
+import kotlinx.coroutines.flow.Flow
+
+class AddItemRepoImpl(private val dao: AddItemDao) : AddItemRepository {
+    override suspend fun insertItem(item: AddItem) {
+        dao.insertItem(item)
+    }
+
+    override suspend fun insertItem(item: ShoppingListItem) {
+        dao.insertItem(item)
+    }
+
+    override suspend fun deleteItem(item: AddItem) {
+        dao.deleteItem(item)
+    }
+
+    override fun getAllItemsById(listId: Int): Flow<List<AddItem>> {
+        return dao.getAllItemsById(listId)
+    }
+
+    override suspend fun getListItemsById(listId: Int): ShoppingListItem {
+        return dao.getListItemsById(listId)
+    }
+}
