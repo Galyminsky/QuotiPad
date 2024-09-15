0cedf5976be870fe254d63546ea079311c8dcbce
Galyaminsky_Linux <jobforandroid@proton.me>
2024-09-15 07:26:19 +0500

    #5 Add AddItemDao

---
+++ app/src/main/java/galyaminsky/quotipad/data/AddItemDao.kt
@@ -0,0 +1,20 @@
+package galyaminsky.quotipad.data
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface AddItemDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertItem(item: AddItem)
+
+    @Delete
+    suspend fun deleteItem(item: AddItem)
+
+    @Query("SELECT * FROM add_item WHERE listId = :listId")
+    fun getAllItemsById(listId: Int): Flow<List<AddItem>>
+}
