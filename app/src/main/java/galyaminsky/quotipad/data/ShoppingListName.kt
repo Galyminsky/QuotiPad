891125b66618247bffe3e9a6a9199b8b35bac6ce
Galyaminsky_Linux <jobforandroid@proton.me>
2024-09-15 06:45:05 +0500

    #2 Entity Review

--- app/src/main/java/galyaminsky/quotipad/data/ShoppingListName.kt
+++ app/src/main/java/galyaminsky/quotipad/data/ShoppingListName.kt
@@ -1,14 +1,15 @@
 package galyaminsky.quotipad.data
+
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "shopping_list_name")
 data class ShoppingListName(
     @PrimaryKey
-    val id: Int,
+    val id: Int? = null,
     val name: String,
     val time: String,
     val allItemsCount: Int,
     val allSelectedItemsCount: Int,
 
-)
+    )
