2dbfa0067fb6178ab18b60da40165d4f7ca01272
Galyaminsky_WIN <jobforandroid@proton.me>
2024-09-17 23:47:11 +0500

    #3 - FloatButton

--- app/src/main/java/galyaminsky/quotipad/main_screen/MainScreen.kt
+++ app/src/main/java/galyaminsky/quotipad/main_screen/MainScreen.kt
@@ -1,21 +1,28 @@
 package galyaminsky.quotipad.main_screen
 
 import android.annotation.SuppressLint
+import androidx.compose.material3.FabPosition
 import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.painterResource
+import galyaminsky.quotipad.R
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
-fun  MainScreen () {
-    Scaffold (bottomBar = {
+fun MainScreen() {
+    Scaffold(bottomBar = {
 
     }, floatingActionButton = {
         FloatingActionButton(onClick = {}) {
-            
+            Icon(
+                painter = painterResource(id = R.drawable.icon_add),
+                contentDescription = "add"
+            )
         }
-    }
-        ) {
+    }, floatingActionButtonPosition = FabPosition.Center
+    ) {
 
     }
 }
